d9d85b8a35eb6ca5c8450161f0db433c95ffe838
tarunshukla23k <131612967+tarunshukla23k@users.noreply.github.com>
2024-05-25 17:03:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let display = document.getElementById('display');
+
+function appendNumber(number) {
+    display.value += number;
+}
+
+function appendOperator(operator) {
+    display.value += operator;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculateResult() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        alert('Invalid Expression');
+        clearDisplay();
+    }
+}
